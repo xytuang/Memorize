cb9e1a299a70c13ddb76d36cd8abd91c5891aaef
Xiang Yu Tuang <xiangyutuang@gmail.com>
2023-09-14 21:39:12 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Xiang Yu Tuang on 14/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
